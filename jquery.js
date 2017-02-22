a174f565582154447109a228fcd30a49d2bf4776
Grace <gracefctorres@gmail.com>
2017-02-22 11:46:04 -0500

    adding jquery-functions and id create span

--- jquery.js
+++ jquery.js
@@ -4,67 +4,71 @@ $(function(){
     $("#input-email").keyup(validaEmail);
     $("#input-password").keyup(validaPassword);
     $("#options").click(validaSelector);
+    
+});
+function validateForm(){
     if (validaName()==true && validaLastName()==true && validaEmail()==true && validaPassword()==true && validaSelector()==true){
         
     }
-});
+}
 
 function createSpan(_id,message){
-  
-    if(_id.find("span").length==0){
-        $(_id).append("<span>"+message+"</span>")
-    }
-        
-    
+    if(_id.find('span').length==0){
+        $(_id).append("<span>"+message+"</span>");
+    } 
 }
 
 var validaName = function validateName(){
     //console.log("creo que no");
     var valName=$("#name").val();
+    var nameCont= $("#name-container");
     var isName= true;
     if(valName.length > 0 && valName.match(/^[a-zA-Z\s]*$/)){
         $("#name-container span").remove();
-        return isName=true; 
-    }else{
-        createSpan("#name-container","Nombre valido");
         return isName=false;
+    }else{
+        createSpan(nameCont,"Coloca tu nombre");
+        return isName=true;
     }
 }
 
 var validaLastName= function validateLastName(){
     var valLastName=$("#lastname").val();
-    console.log(lastname);
+    var lastnameCont= $("#lastname-container");
     var isLastName= true;
-    if(valLastName.length!=0 && valLastName.match(/^[a-zA-Z\s]*$/)){
-        createSpan("#lastname-container","Apellido valido")
-        return isLastName=true; 
-    }else{
+    if(valLastName.length > 0 && valLastName.match(/^[a-zA-Z\s]*$/)){
         $("#lastname-container span").remove();
-        return isLastName=false; 
+        return isLastName=false;
+    }else{
+        createSpan(lastnameCont,"Coloca tu apellido")
+        return isLastName=true; 
     }
 }
 var validaEmail = function validateEmail(){
     var valiMail = $("#input-email");
+    var emailCont= $("#email-container");
     var isEmail= true;
-    if(valiMail.val().length!=0 && valiMail.val().match(/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/)){
-        createSpan("#email-container","Email valido")
-        return isEmail=true; 
-    }else{
+    if(valiMail.val().length>0 && valiMail.val().match(/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/)){
         $("#email-container span").remove();
-        return isEmail=false;}
+        return isEmail=false; 
+    }else{
+        createSpan(emailCont,"Completa con tu email");
+        return isEmail=true;}
 }
  var validaPassword= function validatePassword(){
     var valPassword = $("#input-password").val();
+    var passCont= $("#password-container");
     if(valPassword=="098754"||valPassword=="password"||valPassword=="123456"||valPassword.length<=6){
-            createSpan("#password-container","Ingrese correctamente su contraseña");
+            createSpan(passCont,"Ingrese correctamente su contraseña");
         }else{
             $("#password-container span").remove();}   
 }
 //validating options about bycicles
 var validaSelector= function validateSelector(){
     var indice = $("#options");
-    if(indice.val() == null || indice.val() == 0 ){
-       createSpan("#options","Selecciona una bicicleta");
+    var selectCont= $("#select-container");
+    if(indice.val() != null || indice.val() == 0 ){
+       createSpan(selectCont,"Selecciona una bicicleta");
     }else{
         $("#options span").remove();
     }  
